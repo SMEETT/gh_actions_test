2757712f4bc372285b6d03e631205ac9e83aa48f
SMEETT <tbr@tutamail.com>
2021-08-16 02:54:07 +0200

    changed deploy.yml

--- frontend/main.js
+++ frontend/main.js
@@ -4,6 +4,6 @@ document.querySelector("#app").innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
-fetch("http://localhost:3003")
+fetch("https://testback.borisfries.dev")
 	.then((response) => response.json())
 	.then((data) => console.log(data));
