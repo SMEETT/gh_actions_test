3034502e913e3653c5e192a902ddd7356fc0b363
SMEETT <tbr@tutamail.com>
2021-08-16 14:26:50 +0200

    updated deploy.yml [deploy]

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,37 +1,30 @@
-cd /home/bf/gh_actions_test/backend
 echo "#################################################"
 echo Install: Backend Dependencies
 echo "#################################################"
+cd /home/bf/gh_actions_test/backend
 npm install
-echo "#################################################"
-echo Done: Install Backend Dependencies
-echo "#################################################"
-
 echo "#################################################"
 echo Start: Backend via PM2
 echo "#################################################"
-pm2 stop /home/bf/gh_actions_test/backend/index.js --name "test_backend"
+pm2 delete test_backend
 pm2 start /home/bf/gh_actions_test/backend/index.js --name "test_backend"
-cd /home/bf/gh_actions_test/frontend
 echo "#################################################"
 echo Install: Frontend Dependencies
 echo "#################################################"
+cd /home/bf/gh_actions_test/frontend
 npm install --production=false
-echo "#################################################"
-echo Done: Install Frontend Dependencies
-echo "#################################################"
 echo "#################################################"
 echo Start: Frontend Build
 echo "#################################################"
 npm run build
 echo "#################################################"
-echo Done: Frontend Build
-echo "#################################################"
-pm2 delete test_frontend
-echo "#################################################"
 echo Start: Frontend via PM2
 echo "#################################################"
+pm2 delete test_frontend
 pm2 start npm --name "test_frontend" -- run serve
+echo "#################################################"
+echo Save: PM2
+echo "#################################################"
 pm2 save
 echo "-------------------------------------------------"
 echo "#################################################"
