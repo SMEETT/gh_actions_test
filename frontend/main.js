c7ba557cd7f357ee759174107706219d59555880
SMEETT <tbr@tutamail.com>
2021-08-16 03:03:48 +0200

    hello world (deploy)

--- frontend/main.js
+++ frontend/main.js
@@ -2,6 +2,7 @@ import "./style.css";
 
 document.querySelector("#app").innerHTML = `
   <h1>Hello Vite!</h1>
+  <h1>Hello World!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
 fetch("https://testback.borisfries.dev")
